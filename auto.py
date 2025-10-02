3537896c6eabfc8168d8080f10a1bfafe4d3e7a1
Peiheng Han <100980962+Jaulin-Bage@users.noreply.github.com>
2025-10-02 18:35:36 +0800

    Add files via upload

---
+++ auto.py
@@ -0,0 +1,31 @@
+import os 
+import GPUtil
+import time
+def get_gpu_info():
+   gpulist = []
+   Gpus = GPUtil.getGPUs()
+   GPUtil.showUtilization()
+   for gpu in Gpus:
+      # modify the rules, default 5%.
+      if gpu.load<0.05 and gpu.memoryUtil<0.05:
+         gpulist.append(gpu.id)
+   return gpulist
+import json
+def read_json_file(path):
+    with open(path, 'r') as f:
+        data = json.load(f)
+    return data
+def main():
+   data=read_json_file('todo.json')
+   for i in data:
+      while True:
+         gpu_info = get_gpu_info()
+         if gpu_info != [] and len(gpu_info)<= i['gpu_require']:
+            gpu_use=lambda gpu_info,i: ','.join(map(str, gpu_info[:i['gpu_require']]))
+            print(f"task start")
+            os.system(f"deepspeed --include localhost:{gpu_use} --master_port 29505 {i['task']} > {i['log']}")
+            continue
+         else:
+            time.sleep(10)
+if __name__ == '__main__':
+   main()
